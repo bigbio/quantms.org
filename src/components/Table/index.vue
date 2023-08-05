4e73c254828539cb04471162ceed9501b3501a71
shabai517 <shabai517@gmail.com>
2023-08-05 16:53:49 +0100

    add progress for the table

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -11,9 +11,10 @@
         fontSize: '16px',
       }"
       :cell-style="{}"
-      height="1000"
+      height="600"
+      :default-sort="{ prop: ['peptides','proteins'], order: 'descending' }"
     >
-      <el-table-column label="Accession" min-width="110">
+      <el-table-column label="Accession" width="110">
         <template #default="scope">
           <div style="display: flex; align-items: center">
             <el-link :href="scope.row.accession.path" type="primary">{{
@@ -22,10 +23,35 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column prop="samples" label="Samples" min-width="100" />
-      <el-table-column prop="msruns" label="Msruns" min-width="100" />
-      <el-table-column prop="peptides" label="Peptides" min-width="100" sortable/>
-      <el-table-column prop="proteins" label="Proteins" min-width="100" sortable/>
+      <el-table-column prop="category" label="Category" width="180" />
+      <el-table-column prop="samples" label="Samples" width="100" />
+      <el-table-column prop="msruns" label="Msruns" width="100" />
+      <el-table-column prop="peptides" label="Peptides" min-width="100" sortable >
+        <template #default="scope">
+          <el-tooltip
+            effect="light"
+            :content="scope.row.peptides"
+            :offset="-50"
+            placement="right-start"
+          >
+            <el-progress :percentage="100*scope.row.peptides/maxPeptides" :stroke-width="12" :show-text="false"/>
+          </el-tooltip>
+          
+        </template>
+      </el-table-column>
+      <el-table-column prop="proteins" label="Proteins" min-width="100" sortable  >
+        <template #default="scope">
+          <el-tooltip
+            effect="light"
+            :content="scope.row.proteins"
+            :offset="-50"
+            placement="right-start"
+          >
+            <el-progress :percentage="100*scope.row.proteins/maxProteins" :stroke-width="12" :show-text="false"/>
+          </el-tooltip>
+          
+        </template>
+       </el-table-column> 
 <!--      <el-table-column label="Title" min-width="400">-->
 <!--        <template #default="scope">-->
 <!--          <div style="display: flex; align-items: center">-->
@@ -61,7 +87,11 @@
   </div>
 </template>
 <script setup>
-import { computed, ref } from "vue";
+import { computed, ref, onMounted,watch } from "vue";
+
+const maxPeptides = ref(0);
+const maxProteins = ref(0);
+
 const props = defineProps({
   modelValue: {
     type: Array,
@@ -76,6 +106,17 @@ const props = defineProps({
   },
 });
 
+onMounted(()=>{
+  console.log('onMounted',props.modelValue);
+})
+
+watch(()=>props.modelValue,(val)=>{
+  console.log('watch',val)
+  maxPeptides.value = Math.max(...val.map(o => o.peptides));
+  maxProteins.value = Math.max(...val.map(o => o.proteins));
+  console.log(maxPeptides.value,maxProteins.value);
+})
+
 const total = computed(() => {
   return props.modelValue.length;
 });
@@ -89,19 +130,19 @@ const tableData = computed(() => {
 });
 
 const handleCurrentChange = (val) => {
-  console.log(val);
+  // console.log(val);
   currentPage.value = val;
 };
 </script>
 <style lang="scss" scoped>
 ::v-deep {
   .el-pagination.is-background .el-pager li:not(.disabled) {
-    background-color: white; // 进行修改未选中背景和字体
+    background-color: white; //
     padding: 0 1rem;
     color: black;
   }
   .el-pagination.is-background .el-pager li:not(.disabled).is-active {
-    background-color: rgb(229, 231, 235); // 进行修改选中项背景和字体
+    background-color: rgb(229, 231, 235); //
     padding: 0 1rem;
     border-radius: 0.25rem;
   }
